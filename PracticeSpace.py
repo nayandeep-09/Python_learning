4cc1b33c80b9fd9275505ad0ff381767d69d1f6b
Your Namenayandeep-09 <nayandeepchouhan864@gmail.com>
2026-08-20 21:34:18 +0530

    a simple basic input program

--- PracticeSpace.py
+++ PracticeSpace.py
@@ -1,2 +1,10 @@
 print ("Welcome to Practice Space! This is a simple program to help you practice coding. You can write your code here and test it out. Let's get started!")
 
+input("Press Enter to continue...")
+a = input("Please enter a number: ")
+try:
+    a = int(a)
+    print(f"You entered the number: {a}")
+except ValueError:
+    print("That's not a valid number!") 
+    
